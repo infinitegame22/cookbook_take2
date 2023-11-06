7fb6c08872edc54aa256bfc144c6d08aa6e66d96
Amy Dripchak <rejuvenatingserendipity@gmail.com>
2023-11-06 12:52:07 -0700

    update

--- main.rb
+++ main.rb
@@ -1,15 +1,39 @@
 require 'sinatra'
 
-def load_pictures
-  Dir.glob("public/*.{jpg,JPG}")
-end
+# def load_pictures
+#   Dir.glob("public/*.{jpg,JPG}")
+# end
+
+# get '/' do
+#   @pictures = load_pictures
+#   erb :index
+# end
+
+# post '/image' do
+#   erb :upload
+# end
 
 get '/' do
-  @pictures = load_pictures
-  erb :index
+  erb :homepage
 end
 
-post '/image' do
-  erb :upload
+get "/new" do
+  erb :new
 end
 
+post "/create" do
+  filename = params[:filename].to_s
+
+  if filename.size == 0
+    session[:message] = "A name is required."
+    status 422
+    erb :new
+  else
+    file_path = File.join(data_path, filename)
+
+    File.write(file_path, "")
+    session[:message] = "#{params[:filename]} has been created."
+
+    redirect "/"
+  end
+end
